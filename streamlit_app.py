8aafd2a740a95efdc179ec7cc720e3af406c6ff9
Kezadana <164410525+Kezadana@users.noreply.github.com>
2024-06-02 16:19:45 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import sklearn as sk
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
